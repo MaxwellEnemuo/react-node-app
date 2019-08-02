6e46d3a780c9f49d541d6030b0da75d007cb5352
maxwell enemuo <chinedu.enemuo@gmail.com>
2019-08-02 14:43:43 +0100

    Update db.conn.js

--- config/db.conn.js
+++ config/db.conn.js
@@ -2,18 +2,11 @@ let mongoose = require("mongoose");
 require('dotenv').config()
 
 
-// const host = process.env.DB_HOST;
-// const database = process.env.DB_DATABASE;
-// const username = process.env.DB_USERNAME;
-// const password = process.env.DB_PASSWORD;
-// const port = process.env.DB_PORT;
-
-const host = 'localhost';
-const database = 'mern';
-const username = '';
-const password = '';
-const port = 27017;
-
+const host = process.env.DB_HOST;
+const database = process.env.DB_DATABASE;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT;
 
 mongoose
   .connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, {
